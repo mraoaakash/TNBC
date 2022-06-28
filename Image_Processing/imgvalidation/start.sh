04e87d05ac6eacc9f817b8c5940fbd443432049d
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-06-28 13:28:54 +0530

    Image val start.sh added

--- Image_Processing/imgvalidation/start.sh
+++ Image_Processing/imgvalidation/start.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-#PBS -N TNB_IMG_split
+#PBS -N TNB_IMG_valid
 #PBS -o out.log
 #PBS -e err.log
 #PBS -l ncpus=104
@@ -7,4 +7,4 @@
 
 module load compiler/anaconda3
 source /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgsplit/env/bin/activate
-python3 /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgsplit/code.py
+python3 /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgvalidation/code.py
