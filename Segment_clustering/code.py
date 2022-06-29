e9d7b37f2dff7fd3c41527b252aea05d56f8d0b6
Aakash Rao <mrao.aakash@gmail.com>
2022-06-29 15:50:44 +0530

    Misc commit

--- Segment_clustering/code.py
+++ Segment_clustering/code.py
@@ -1,26 +1,26 @@
+from datetime import datetime
+import time
 import imagecodecs
 import shutil
 import tifffile as tiff
 import os
+import fnmatch
 import numpy as np
 
 def create_dataset(path):
 	imlist=[]
 	labels=[]
-	n=200000
-	for file in os.listdir(path)[:n]:
-		linker = str(os.path.join(path, file))
-		img = tiff.imread(linker)
-		imlist.append(list(np.array(img).reshape(-1)))
-		labels.append(str(linker))
+	for file in os.listdir(path):
+		if fnmatch.fnmatch(file, '*.tif'):
+			linker = str(os.path.join(path, file))
+			img = tiff.imread(linker)
+			imlist.append(list(np.array(img).reshape(-1)))
+			labels.append(str(linker))
 	imlist = np.array(imlist)
-	print(imlist)
-	print(labels)
-	np.savetxt("/home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Segment_clustering/images.csv",imlist, delimiter=',')
-	with open('/home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Segment_clustering/labels.txt', 'w+') as f:
+	np.savetxt("/home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Segment_clustering/images_625.csv",imlist, delimiter=',')
+	with open('/home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Segment_clustering/labels_625.txt', 'w+') as f:
 		for line in labels:
 			f.write(line)
 			f.write('\n')
 
-
-create_dataset("/storage/tnbc/segments/224:224/Raw")
+create_dataset("/storage/tnbc/segments/224:224/Send")
