32a10ec67c130c11d5c9e8c214d9222cb36fec67
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-07-08 15:06:43 +0530

    Modified code for imgsplit

--- Image_Processing/imgsplit/grid_im_split.py
+++ Image_Processing/imgsplit/grid_im_split.py
@@ -14,15 +14,37 @@ import numpy as np
 exclude = ['benchmark','segments']
 exclude_im=["20200220-515-191_17-1049_17A-HER2-Biopsy-HnE-40X.tif","20200220-161-112_15-619_15_A-HER2-Biopsy-HnE-40X.tif", "20200220-142-246_15-3385_15_A-HER2-Biopsy-HnE-40X.tif", "20210821_752_307_19_W3_19_H7_ER_Surgery_HnE_40X.tif", "20210821_897_497_19_1816_19_A_ER_biopsy_HnE_40X.tif"]
 
-def new_randcrop():
+def new_randcrop(file, filename, x=0):
 
     size=224
     filekey = dict()
-    x=19800
+    path=file
+    img = tiff.imread(path,0) #brings the image into memory
+    print(filename)
+    for i in range(0,img.shape[0]-size,+img.shape[0]//10):
+        for j in range(0, img.shape[1]-size, +img.shape[1]//10):
+            random.seed(x)
+            w = i+ randint(0,img.shape[0]//10-size)
+            random.seed(x)
+            l = j+ randint(0,img.shape[1]//10-size)
+            crop = img[w:(w+size) ,l:(l+size)] #crops size squared area around the defined random coordinate
+            newfilename = "patch_"+str(x)+".tif"
+            filepath = "/storage/tnbc/segments/newseg/224/"+newfilename
+            cv2.imwrite(filepath, crop) #saves the image with the filepath mentioned above
+            command = "md5sum " +  filepath
+            sum = os.popen(command).read()
+            filekey[newfilename] = {"name":filename, "oldpath": path, "newpath":filepath, "x":w, "y":l, "seed":x, "size":size, "im_shape":img.shape, "md5sum":sum}
+            x+=1
+    with open("/storage/tnbc/segments/newseg/224/metadata_224.json", "w+") as outfile:
+        json.dump(filekey, outfile)
+
+new_randcrop()
+
+
+def caller():
     dir = "/storage/tnbc"
     key="hne"
     array = []
-    dir = "/storage/tnbc"
 
     for subdir, dirs, files in os.walk(dir):
         dirs[:] = [d for d in dirs if d not in exclude]
@@ -32,28 +54,7 @@ def new_randcrop():
                 if key in file.lower():
                     prpath=str(os.path.join(subdir, file))
                     array.append((prpath, file))
-    files = file[199:]
-    for file in array:
-        path=file[0]
-        img = tiff.imread(path,0) #brings the image into memory
-        print(str(file[1]))
-        for i in range(0,img.shape[0]-size,+img.shape[0]//10):
-            for j in range(0, img.shape[1]-size, +img.shape[1]//10):
-                random.seed(x)
-                w = i+ randint(0,img.shape[0]//10-size)
-                random.seed(x)
-                l = j+ randint(0,img.shape[1]//10-size)
-                crop = img[w:(w+size) ,l:(l+size)] #crops size squared area around the defined random coordinate
-                newfilename = "patch_"+str(x)+".tif"
-                filepath = "/storage/tnbc/segments/newseg/224/"+newfilename
-                cv2.imwrite(filepath, crop) #saves the image with the filepath mentioned above
-                command = "md5sum " +  filepath
-                sum = os.popen(command).read()
-                filekey[newfilename] = {"name":file[1], "oldpath": path, "newpath":filepath, "x":w, "y":l, "seed":x, "size":size, "im_shape":img.shape, "md5sum":sum}
-                x+=1
-    with open("/storage/tnbc/segments/newseg/224/metadata_224.json", "w+") as outfile:
-        json.dump(filekey, outfile)
-
-new_randcrop()
-
-
+    j=0
+    for i in array:
+        new_randcrop(i[0], i[1], j*100)
+        j+=1
