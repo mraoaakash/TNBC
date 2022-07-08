974352db8c680f1e66b31fe899c33beb9e72f434
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-07-08 14:38:26 +0530

    A

--- Image_Processing/imgsplit/start.sh
+++ Image_Processing/imgsplit/start.sh
@@ -7,4 +7,4 @@
 
 module load compiler/anaconda3
 source /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgsplit/env/bin/activate
-python3 /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgsplit/grid_im_split.pygit
+python3 /home/aakash.rao_ug23/TNBC/gitrepo/tnbc/Image_Processing/imgsplit/grid_im_split.py
