82f1bc9b46545b2b01f4c6955a2fd46bffa5eef5
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-07-08 15:22:16 +0530

    debug

--- Image_Processing/imgsplit/grid_im_split.py
+++ Image_Processing/imgsplit/grid_im_split.py
@@ -40,7 +40,7 @@ def new_randcrop(file, filename, x=0):
 def caller():
     dir = "/storage/tnbc"
     key="hne"
-    filekey=dict()
+    filekey=dict({})
     array = []
     j=0
     for subdir, dirs, files in os.walk(dir):
