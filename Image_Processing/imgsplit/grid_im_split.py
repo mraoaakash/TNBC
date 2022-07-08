53c5ef15b117773c442d180fe84c1ca42e60c59d
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-07-08 15:18:15 +0530

    restructured the code

--- Image_Processing/imgsplit/grid_im_split.py
+++ Image_Processing/imgsplit/grid_im_split.py
@@ -12,14 +12,13 @@ import numpy as np
 
 
 exclude = ['benchmark','segments']
-exclude_im=["20200220-515-191_17-1049_17A-HER2-Biopsy-HnE-40X.tif","20200220-161-112_15-619_15_A-HER2-Biopsy-HnE-40X.tif", "20200220-142-246_15-3385_15_A-HER2-Biopsy-HnE-40X.tif", "20210821_752_307_19_W3_19_H7_ER_Surgery_HnE_40X.tif", "20210821_897_497_19_1816_19_A_ER_biopsy_HnE_40X.tif"]
+exclude_im=["20200220-515-191_17-1049_17A-HER2-Biopsy-HnE-40X.tif","20200220-161-112_15-619_15_A-HER2-Biopsy-HnE-40X.tif", "20200220-142-246_15-3385_15_A-HER2-Biopsy-HnE-40X.tif", "20210821_752_307_19_W3_19_H7_ER_Surgery_HnE_40X.tif"]
 
 def new_randcrop(file, filename, x=0):
 
     size=224
     filekey = dict()
-    path=file
-    img = tiff.imread(path,0) #brings the image into memory
+    img = tiff.imread(file,0) #brings the image into memory
     print(filename)
     for i in range(0,img.shape[0]-size,+img.shape[0]//10):
         for j in range(0, img.shape[1]-size, +img.shape[1]//10):
@@ -33,17 +32,16 @@ def new_randcrop(file, filename, x=0):
             cv2.imwrite(filepath, crop) #saves the image with the filepath mentioned above
             command = "md5sum " +  filepath
             sum = os.popen(command).read()
-            filekey[newfilename] = {"name":filename, "oldpath": path, "newpath":filepath, "x":w, "y":l, "seed":x, "size":size, "im_shape":img.shape, "md5sum":sum}
+            filekey[newfilename] = {"name":filename, "oldpath": file, "newpath":filepath, "x":w, "y":l, "seed":x, "size":size, "im_shape":img.shape, "md5sum":sum}
             x+=1
-    with open("/storage/tnbc/segments/newseg/224/metadata_224.json", "w+") as outfile:
-        json.dump(filekey, outfile)
+    return filekey
 
 
 def caller():
     dir = "/storage/tnbc"
     key="hne"
     array = []
-
+    j=0
     for subdir, dirs, files in os.walk(dir):
         dirs[:] = [d for d in dirs if d not in exclude]
         files[:] = [f for f in files if f not in exclude_im]
@@ -51,11 +49,10 @@ def caller():
             if fnmatch.fnmatch(file, '*.tif'):
                 if key in file.lower():
                     prpath=str(os.path.join(subdir, file))
-                    array.append((prpath, file))
-    print(array)
-    j=0
-    for i in array:
-        new_randcrop(i[0], i[1], j*100)
-        j+=1
+                    batch_keys = new_randcrop(prpath, file, j*100)
+                    j+=1
+                    filekey = {**filekey, **batch_keys}
+    with open("/storage/tnbc/segments/newseg/224/metadata_224.json", "w+") as outfile:
+        json.dump(filekey, outfile)
 
 caller()
