58316f74e0518e6b236486e43a40e289b13a110e
Aakash Rao <69803820+mraoaakash@users.noreply.github.com>
2022-07-21 11:28:46 +0530

    Debugging the file retreival code

--- Image_Processing/imgret/imgRet.py
+++ Image_Processing/imgret/imgRet.py
@@ -2,14 +2,13 @@ import shutil # for copying files
 import os # for checking if file exists
 import numpy as np # for image processing
 import fnmatch # for matching file names
-
+exclude = ['benchmark','segments', 'ex_datasets', 'tSNE', 'newseg', '224:224', 'send_og', 'clusters']
 def imgRet(key='hne'):
     for subdir, dirs, files in os.walk("/storage/tnbc"):
-        exclude = ['benchmark','segments', 'ex_datasets', 'tSNE']
         dirs[:] = [d for d in dirs if d not in exclude]
         for file in files:
             if fnmatch.fnmatch(file, '*.tif'):
-                if key in file.lower():
+                if key in file.lower() and (("cropped" in file.lower())==False):
                     prpath=str(os.path.join(subdir, file))
                     print(file)
                     shutil.copy(prpath, "/storage/tnbc/segments/224:224/send_og")
